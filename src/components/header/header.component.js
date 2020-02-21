a7ea0fda9434dc588be70d21caa1634bcf992338
Jean <jean.soro007@gmail.com>
2020-02-21 16:50:55 -0500

    Implement Redux store, user actions, user reducer and root reducer functions

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import './header.styles.scss';
 import { ReactComponent as Logo } from './crown.svg'
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -26,4 +27,8 @@ const Header = ({ currentUser }) => (
   </div>
 )
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
