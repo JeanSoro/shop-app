ac6c302ff856c621da52d0efbad0896454db7912
Jean <jean.soro007@gmail.com>
2020-02-24 13:44:26 -0500

    Refactor to use reselect library to implement memoization and avoid unecessary rerenders in Cart DropDown Component

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -2,17 +2,28 @@ import React from 'react';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from './shopping-bag.svg';
 import { connect } from 'react-redux';
-import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartIcon = ({ toggleCartHidden }) => (
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
 //Fires CART.ACTION
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
-})
-export default connect(null, mapDispatchToProps)(CartIcon);
+});
+
+
+//redux selector
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
+});
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -6,8 +6,6 @@ export const toggleCartHidden = () => ({
 })
 
 
-
-
 export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item
